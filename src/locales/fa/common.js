b3edc9c6c99dbf1fa6ab9b014f162984eb98885a
timothycarambat <rambat1010@gmail.com>
2025-04-27 10:33:33 -0700

    forgot translation files

---
+++ src/locales/fa/common.js
@@ -0,0 +1,9 @@
+// Anything with "null" requires a translation. Contribute to translation via a PR!
+const TRANSLATIONS = {
+  chat: {
+    "send-message": null,
+    "reset-chat": null,
+  },
+};
+
+export default TRANSLATIONS;
